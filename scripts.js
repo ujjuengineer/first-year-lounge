588cbbfe48427e90e2a01f3e53f21670f48f3026
ujjuengineer <uk3945351@gmail.com>
2024-12-01 15:15:41 +0530

    updated home page

--- scripts.js
+++ scripts.js
@@ -7,3 +7,72 @@ function sendMail(){
 
     emailjs.send("service_o7krp5l", "template_pcazqis", parms).then(event.preventDefault(), alert("Email sent!!"))
 }
+
+// Function to wrap each word in a span
+function wrapWords(paragraph) {
+    const text = paragraph.innerText;
+    const words = text.split(" ");
+    paragraph.innerHTML = ""; 
+
+    // ensuring spaces are properly handled
+    words.forEach((word, index) => {
+        const span = document.createElement("span");
+        span.classList.add("zoom-text");
+        span.innerText = word; 
+        paragraph.appendChild(span);
+
+        // ignoring the last word
+        if (index < words.length - 1) {
+            paragraph.appendChild(document.createTextNode(" "));
+        }
+    });
+}
+
+const paragraphs = document.querySelectorAll('.zoom-paragraph');
+
+paragraphs.forEach(para => {
+    wrapWords(para);
+});
+
+
+const zoomTextElements = document.querySelectorAll('.zoom-text');
+
+
+zoomTextElements.forEach((element, index) => {
+    element.addEventListener('mouseenter', () => {
+        // Reset all the zoom levels and colors
+        zoomTextElements.forEach(el => {
+            el.classList.remove("zoomed", "adjacent");
+        });
+
+        // Apply zoom and color change to the hovered word
+        element.classList.add("zoomed");
+
+        
+        if (index > 0) {
+            zoomTextElements[index - 1].classList.add("adjacent");
+        }
+        if (index < zoomTextElements.length - 1) {
+            zoomTextElements[index + 1].classList.add("adjacent");
+        }
+    });
+
+    // Reset the zoom and color change when the mouse leaves the text
+    element.addEventListener('mouseleave', () => {
+        zoomTextElements.forEach(el => {
+            el.classList.remove("zoomed", "adjacent");
+        });
+    });
+});
+
+// sticky class to navbar
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    const header = document.querySelector('.header');
+    
+    if (window.scrollY > header.offsetHeight) {
+        navbar.classList.add('sticky');
+    } else {
+        navbar.classList.remove('sticky');
+    }
+});
